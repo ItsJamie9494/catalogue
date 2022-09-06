328b7cf93633c80a6b634110e2e287f1ffe73e22
Jamie Murphy <hello@itsjamie.dev>
2022-09-06 03:05:19 -0700

    utils: Add more utility functions

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -20,7 +20,7 @@
 
 use std::{
     error::Error,
-    fs::{metadata, read_dir, remove_file},
+    fs::{metadata, read_dir, remove_dir, remove_file},
     path::Path,
     time::SystemTime,
 };
@@ -30,7 +30,15 @@ use log::error;
 /// Remove all contents of a directory, without removing the directory itself
 pub fn remove_dir_contents<P: AsRef<Path>>(path: P) -> Result<(), Box<dyn Error>> {
     for entry in read_dir(path)? {
-        remove_file(entry?.path())?;
+        let entry = entry?;
+        let path = entry.path();
+
+        if entry.file_type()?.is_dir() {
+            remove_dir_contents(&path)?;
+            remove_dir(path)?;
+        } else {
+            remove_file(path)?;
+        }
     }
     Ok(())
 }
@@ -48,4 +56,35 @@ pub fn get_file_age<P: AsRef<Path>>(path: P) -> Result<u64, Box<dyn Error>> {
     }
 
     Ok(u64::MAX)
+}
+
+pub mod xml {
+    use std::{
+        error::Error,
+        fs::File,
+        io::{prelude::*, BufRead, BufReader, LineWriter},
+        path::PathBuf,
+    };
+
+    pub fn fixup(name: &String, source: PathBuf, dest: PathBuf) -> Result<(), Box<dyn Error>> {
+        let mut reader = BufReader::new(File::open(source)?);
+        let mut buf = vec![];
+        let mut writer = LineWriter::new(File::create(dest)?);
+
+        while let Ok(_) = reader.read_until(b'\n', &mut buf) {
+            if buf.is_empty() {
+                break;
+            }
+            let line = String::from_utf8_lossy(&buf);
+            let mut bytes = line.to_string();
+
+            if line.contains("<components version=\"0.8\" origin=\"flatpak\">") {
+                bytes = line.replace("flatpak", name);
+            }
+            writer.write_all(bytes.as_bytes())?;
+            buf.clear();
+        }
+
+        Ok(())
+    }
 }
