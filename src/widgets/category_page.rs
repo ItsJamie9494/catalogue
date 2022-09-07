987a466c69522018df5f4981d5d63a39e2c31811
Jamie Murphy <hello@itsjamie.dev>
2022-09-06 23:34:59 -0700

    CategoryPage: Remove subclass and change widget type

--- src/widgets/category_page.rs
+++ src/widgets/category_page.rs
@@ -29,10 +29,8 @@ mod imp {
     use std::cell::RefCell;
 
     use glib::{ParamSpec, ParamSpecObject, Value};
-    use gtk::FlowBox;
+    use gtk::{BinLayout, FlowBox, Widget};
     use once_cell::sync::Lazy;
-
-    use crate::widgets::page::{Page, PageImpl};
 
     use super::*;
 
@@ -53,10 +51,12 @@ mod imp {
     impl ObjectSubclass for CategoryPage {
         const NAME: &'static str = "CatalogueCategoryPage";
         type Type = super::CategoryPage;
-        type ParentType = Page;
+        type ParentType = Widget;
 
         fn class_init(klass: &mut Self::Class) {
             Self::bind_template(klass);
+
+            klass.set_layout_manager_type::<BinLayout>();
         }
 
         fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
@@ -91,14 +91,19 @@ mod imp {
                 _ => unimplemented!(),
             }
         }
+
+        fn dispose(&self, buildable: &Self::Type) {
+            while let Some(child) = buildable.first_child() {
+                child.unparent();
+            }
+        }
     }
     impl WidgetImpl for CategoryPage {}
-    impl PageImpl for CategoryPage {}
 }
 
 glib::wrapper! {
     pub struct CategoryPage(ObjectSubclass<imp::CategoryPage>)
-        @extends gtk::Widget, gtk::Button;
+        @extends gtk::Widget;
 }
 
 impl CategoryPage {
